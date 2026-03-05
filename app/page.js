a0a3899b7cfdf62d75b5cfea3c769fe1186100ba
kojaekyun <jaekyun.ko@motiv-i.com>
2026-03-05 10:36:07 +0900

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>My Ad Archive</h1>
+      <p>Google Sheets 연결 테스트 중</p>
+      <p>/api/test 로 접속해보세요</p>
+    </div>
+  );
+}
